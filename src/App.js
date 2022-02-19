ffcf70be393a7e7a3ff1dff6bc24fa501aef745b
peter-toson <100045570+peter-toson@users.noreply.github.com>
2022-02-19 21:27:38 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import "./styles.css";
-import PokemonTracker from "./tracker.js";
+import './styles.css';
+//import PokemonTracker from "./tracker.js";
+import { PokemonButton, la_bingo_dex } from './components/pokedex.js';
 
 export default function App() {
-  return <PokemonTracker />;
+    return <PokemonButton pokemon={la_bingo_dex[0]} onClickCallback={(pokemon) => console.log(pokemon.name)} />;
+    //return <PokemonTracker />;
 }
