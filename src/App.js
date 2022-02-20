8d61f5a323ca41255b625243066bc4496641a0b6
peter-toson <100045570+peter-toson@users.noreply.github.com>
2022-02-20 03:41:58 +0100

    quick clean up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './styles.css';
-//import PokemonTracker from "./tracker.js";
 import { la_bingo_dex, types_gen6 } from './components/pokedex.js';
 import { BingoTracker } from './components/bingo-tracker.js';
 
 export default function App() {
-    //return <PokemonTracker />;
-    //return <PokemonButton pokemon={la_bingo_dex[0]} onClickCallback={(pokemon) => console.log(pokemon.name)} />;
     return <BingoTracker pokedex={la_bingo_dex} types={types_gen6} />;
 }
