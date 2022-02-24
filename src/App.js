7ea1b5bcbcac6213c78a37afccb3cfb4c485299d
peter-toson <100045570+peter-toson@users.noreply.github.com>
2022-02-24 01:04:58 +0100

    PictureDex is working!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './styles.css';
-import { la_bingo_dex, types_gen6 } from './components/pokedex.js';
+import { types_gen6 } from './components/pokedex.js';
+import { la_dex_no_legends } from './components/picturedex.js';
 import { BingoTracker } from './components/bingo-tracker.js';
-
-import { PicturedexLA } from './components/picturedex.js';
 
 export default function App() {
     return (
         <>
-            <PicturedexLA />
-            <BingoTracker pokedex={la_bingo_dex} types={types_gen6} />
+            <BingoTracker pokedex={la_dex_no_legends.slice(0, 20)} types={types_gen6} />
         </>
     );
 }
